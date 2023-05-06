498efb58683e37b97ea4ed922bcd97e4241f25c0
Maíra Mél <124319006+MSilva-mel@users.noreply.github.com>
2023-05-06 17:59:50 -0300

    Update movimentacao.js

--- movimentacao.js
+++ movimentacao.js
@@ -1,18 +1,51 @@
-var posicaoX = 1; // posição X inicial do personagem
-var posicaoY = 1; // posição Y inicial do personagem
+let matriz = [];
+
+//funcao para criar um elemento
+function criaTag(elemento) {
+    return document.createElement(elemento);
+}
+
+let tabela = document.getElementById("tabela");
+
+let tbody = criaTag("tbody");
+
+tabela.appendChild(tbody);
+
+// Loop para criar as linhas
+for(let linha = 0; linha < 16; linha++) {
+    let tr = criaTag("tr");
+    tbody.appendChild(tr);
+    // Loop para criar as células em cada linha
+    for(let coluna = 0; coluna < 16; coluna++){
+        let td = criaTag("td");
+        if (linha === 15 || linha === 0 || coluna === 0 || coluna == 15) {
+            td.textContent = "*";
+          } 
+          else if( linha === 1 && coluna === 1) {
+            td.textContent = "&" ;
+        }
+          else {
+            td.textContent = "";
+          }
+        tr.appendChild(td);
+    }
+}
+
+var posicaoX = 1;
+var posicaoY = 1;
 
 document.onkeydown = function(event) {
   switch (event.keyCode) {
-    case 65: // A
+    case 65:
       esquerda();
       break;
-    case 87: // W
+    case 87:
       cima();
       break;
-    case 68: // D
+    case 68:
       direita();
       break;
-    case 83: // S
+    case 83:
       baixo();
       break;
   }
@@ -31,31 +64,45 @@ function cima() {
 }
 
 function direita() {
-  if (posicaoX < 14 && !parede(posicaoX + 1, posicaoY)) {
+  if (posicaoX < 15 && !parede(posicaoX + 1, posicaoY)) {
     setPosition(posicaoX + 1, posicaoY);
   }
 }
 
 function baixo() {
-  if (posicaoY < 14 && !parede(posicaoX, posicaoY + 1)) {
+  if (posicaoY < 15 && !parede(posicaoX, posicaoY + 1)) {
     setPosition(posicaoX, posicaoY + 1);
   }
 }
 
+function setPosition(x, y) {
+  var tabela = document.getElementById("tabela");
+  var linhas = tabela.getElementsByTagName("tr");
+  
+  // Remove o elemento "&" da posição anterior
+  var cellAnterior = linhas[posicaoY].getElementsByTagName("td")[posicaoX];
+  cellAnterior.classList.remove("&");
+  
+  // Atualiza a posição do personagem
+  posicaoX = x;
+  posicaoY = y;
+  
+  // Adiciona o elemento "&" na nova posição
+  var cellNova = linhas[y].getElementsByTagName("td")[x];
+  cellNova.classList.add("&");
+}
+
 function parede(x, y) {
-  var table = document.getElementById("map");
-  var linhas = table.getElementsByTagName("tr");
+  var tabela = document.getElementById("tabela");
+  var linhas = tabela.getElementsByTagName("tr");
   var cell = linhas[y].getElementsByTagName("td")[x];
   return cell.innerHTML === "*";
 }
 
-function setPosition(x, y) {
-  var table = document.getElementById("map");
-  var linhas = table.getElementsByTagName("tr");
-  var cell = linhas[y].getElementsByTagName("td")[x];
-  cell.innerHTML = "&";
-  var oldCell = linhas[posicaoY].getElementsByTagName("td")[posicaoX];
-  oldCell.innerHTML = "";
-  posicaoX = x;
-  posicaoY = y;
+function coletarChave() {
+  chaveColetada = true;
+  var tabela = document.getElementById("tabela");
+  var linhas = tabela.getElementsByTagName("tr");
+  var cell = linhas[posicaoChaveY].getElementsByTagName("td")[posicaoChaveX];
+  cell.innerHTML = "";
 }
